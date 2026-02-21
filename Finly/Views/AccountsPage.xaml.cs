8173761b7de4d886b00067229b1d929b69ff7184
Chumbra <sovesnovkirill@gmail.com>
2026-02-21 23:57:23 +0700

    ff

--- Finly/Views/AccountsPage.xaml.cs
+++ Finly/Views/AccountsPage.xaml.cs
@@ -4,8 +4,21 @@ using Microsoft.Maui.Controls.Xaml;
 
 namespace Finly.Views
 {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AccountsPage : ContentPage
     {
-       
+        private readonly AccountsViewModel _viewModel;
+
+        public AccountsPage(AccountsViewModel viewModel)
+        {
+            InitializeComponent();
+            BindingContext = _viewModel = viewModel;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.LoadAccountsCommand.Execute(null);
+        }
     }
 }
